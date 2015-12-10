e0aa123e2e4b544cb1761916eeacf83dbdbc2051
Danielle <scherrda@msu.edu>
2015-12-09 22:42:14 -0500

    gain is complete

--- project3/main.cpp
+++ project3/main.cpp
@@ -19,12 +19,15 @@
 using std::cout; using std::cin; using std::endl;
 using std::string; using std::vector; using std::ifstream;
 using std::istringstream; using std::stringstream;
-using std::map; using std::remove;
+using std::map; using std::remove; using std::make_pair;
+using std::pair;
 
-int countInstances(vector<string> data, int attributeCount, string desired, int desiredPos)
+
+
+pair<int, int> countInstances(vector<string> data, int attributeCount, string desired, int desiredPos)
 {
 	cout << "number of attributes: " << attributeCount << ", looking for: " << desired << " at " << desiredPos << endl;
-	int positive = 0;
+	int present = 0, positive = 0;
 	int totalData = data.size();
 	for (int i = 0; i < totalData; i++)
 	{
@@ -38,47 +41,69 @@ int countInstances(vector<string> data, int attributeCount, string desired, int 
 			// cout << "value: " << value << " at " << j << endl;
 			if (j == desiredPos)
 			{
-				cout << "checking to match " << desired.c_str() << endl;
+				// cout << "checking to match " << desired.c_str() << endl;
 				if (strncmp (value.c_str(), desired.c_str(), 1) == 0)
 				{
+					if (j != attributeCount)
+					{
+						// cout << "not at end, but match, checking for positive";
+						string boolean = "";
+						while(getline(iss, boolean, ',')){};
+						// cout << " boolean " << boolean << endl;
+						if (strncmp (boolean.c_str(), "T",1) == 0) positive++;
+					}
 					// cout << "TRUE" << endl;
-					positive++;
+					present++;
 				}
 				break;
 			}
 		}
 	}
-	cout << desired << " was present " << positive << " out of " << totalData << endl;
+	// cout << desired << " was present " << present << " out of " << totalData << " and positive " << positive << endl;
 
-	return positive;
+	return make_pair(present, positive);
 }
 
 float CalculateLogValue(float frac1, float frac2)
 {
-	return (-frac1*log2(frac1) + -frac2*log2(frac2));
+	cout << "frac1 " << frac1 << endl;
+	cout << "frac2 " << frac2 << endl;
+
+	return ((frac1 == 0) ? 0 : -frac1*log2(frac1) + ((frac2 == 0) ? 0 : -frac2*log2(frac2)));
 }
 
 float CalculateEntropy(vector<string> data, int attributeCount)
 {
-	int trueCount = countInstances(data, attributeCount, "T", attributeCount);
-	float trueFrac = (float)trueCount/float(data.size());
+	pair<int, int> trueCount = countInstances(data, attributeCount, "T", attributeCount);
+	float trueFrac = (float)trueCount.first/float(data.size());
 	// cout << " frac: " << trueFrac << " log: " << CalculateLogValue(trueFrac, 1-trueFrac) << endl;
 	return CalculateLogValue(trueFrac, 1-trueFrac);
 }
 
-float CalculateInformationGain(vector<string> data, int attributeCount, vector<string> attributes)
+float CalculateInformationGain(vector<string> data, int attributeCount, vector<string> attributes, int index)
 {
 	// cout << "info gain for: " << attribute << endl;
 	// vector<string> vecAtt = attributeValues[attribute];
+	float attributeGain = 0;
 
 	for (int i = 0; i < attributes.size(); i++)
 	{
 		cout << "    calculating for: " << attributes[i] << endl;
+		pair<int, int> attributeTrueCount = countInstances(data, attributeCount, attributes[i], index);
+		cout << attributes[i] << " was present " << attributeTrueCount.first << " out of " << data.size() << " and positive " << attributeTrueCount.second << endl;
+		float attributeFrac = (float)attributeTrueCount.first/float(data.size());
+		// cout << "attributeFrac: " << attributeFrac << endl;
+		float logFrac = (float)(attributeTrueCount.second/(float)attributeTrueCount.first);
+		float logVal = CalculateLogValue(logFrac, 1-logFrac);
+		// cout << "logVal: " << logVal << endl;
+		attributeGain += attributeFrac * logVal;
+		cout << "new gain " << attributeGain << endl;
+
 	}
-	// int attributeTrueCount = countInstances(data, attributeCount, "SMALL", 0);
-	// float attributeFrac = (float)attributeTrueCount/float(data.size());
-	// return CalculateLogValue(attributeFrac, 1-attributeFrac);
-	return 5;
+	cout << "total gain " << attributeGain << endl;
+
+
+	return attributeGain;
 }
 
 void GenerateDecisionTree(vector<string> attributes, int attributeCount, map<string, vector<string>> attributeValues, vector<string> data)
@@ -88,20 +113,29 @@ void GenerateDecisionTree(vector<string> attributes, int attributeCount, map<str
 
 
 	//for all keys in map
-	for (map<string,vector<string>>::iterator it=attributeValues.begin(); it!=attributeValues.end(); ++it)
-	{
-    	cout << "iterating " << it->first << endl;
+	// for (map<string,vector<string>>::iterator it=attributeValues.begin(); it!=attributeValues.end(); ++it)
+	// {
+    // 	cout << "iterating " << it->first << endl;
 		// for (int i = 0; i < it->second.size(); i++)
 		// {
 			// cout << "    element: " << it->second[i] << endl;
-			float gain = CalculateInformationGain(data, attributeCount, it->second);
 
+
+			cout << "calculating info gain" << endl;
+
+			float gainSize = CalculateInformationGain(data, attributeCount, attributeValues["size"], 0);
+			cout << endl << endl;
+			float gainColor = CalculateInformationGain(data, attributeCount, attributeValues["color"], 1);
+			cout << endl << endl;
+			float gainShape = CalculateInformationGain(data, attributeCount, attributeValues["shape"], 2);
+
+			// float gainSizeDiff = entropy - gainSize;
 			// cout << "gain for: " << it->second[i] << " (not really) is " << gain << endl;
 
 		// }
 		// cout << "gain for: " << attributeValues[0] << " (not really) is " << gain << endl;
 
-	}
+	// }
 
 	// for (int i = 0; i < attributes.size(); i++)
 	// {
@@ -155,7 +189,7 @@ int main(int argc, char * argv[])
 					// getline(infile, nextLineWord, ' ');
 					getline(iss, nextWord, ' ');
 					attributeName = nextWord;
-					cout << "    ATTRIBUTE name: " << attributeName << endl;
+					// cout << "    ATTRIBUTE name: " << attributeName << endl;
 					// attributes.push_back(attributeName);
 					categoryName = nextWord;
 					// cout << "    CATEGORY NAME: " << categoryName << endl;
@@ -165,7 +199,7 @@ int main(int argc, char * argv[])
 						remove(nextWord.begin(), nextWord.end(), '{');
 						remove(nextWord.begin(), nextWord.end(), '}');
 						nextWord.erase(remove(nextWord.begin(), nextWord.end(), ','), nextWord.end());
-						cout << "        word:" << nextWord << endl;
+						// cout << "        word:" << nextWord << endl;
 						attributes.push_back(nextWord);
 
 					}
